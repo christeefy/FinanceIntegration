6eba7a9da5780637637a7388441dc47ba7e20af7
christeefy <fuyuan.tee@mail.utoronto.ca>
2018-05-01 22:04:42 -0400

    Include .roi method

--- asset/currency.py
+++ asset/currency.py
@@ -15,7 +15,7 @@ class Asset(metaclass=abc.ABCMeta):
 class Currency(Asset, metaclass=abc.ABCMeta):
     
     transaction_schema = ['datetime', 'type', 'units', 'price', 'fee']
-    transaction_types = ['fund', 'buy', 'comm', 'fee', 'sell', 'transfer']
+    transaction_types = ['fund', 'buy', 'comm', 'fee', 'sell', 'transfer', 'display']
     
     def __init__(self, name, symbol):
         self.name = name
@@ -24,6 +24,7 @@ class Currency(Asset, metaclass=abc.ABCMeta):
         self.total_profit = 0.
         self.current_acb = 0.
         self.transactions = pd.DataFrame()
+        self.inception_date = None
 
     def __repr__(self):
         return f'{self.name} ({self.symbol})\nUnits: {self.units:.5f}\nTotal Profit: {round(self.total_profit, 2)}'
@@ -240,6 +241,10 @@ class Currency(Asset, metaclass=abc.ABCMeta):
 
     ## Private methods for handling transactions
     def _add_transaction(self, transaction_type, units, price, fee, dt):
+        # Add inception date for first transaction it encounters
+        if self.inception_date is None:
+            self.inception_date = dt
+
         self.transactions = pd.concat([self.transactions, 
                                        pd.DataFrame([(dt, transaction_type, units, price, fee)], 
                                                     columns=Currency.transaction_schema)],
@@ -265,6 +270,209 @@ class Currency(Asset, metaclass=abc.ABCMeta):
                   .reset_index())
 
         return df
+
+
+    ## Private static method for ROI calculations
+    @staticmethod
+    def _decouple_fees(raw_df):
+        ''' DataFrame -> DataFrame
+        Decouple the fees from transactions by creating a dedicated 'type'
+        for fees.
+
+        Return a new dataframe.
+        '''
+        keys = ['datetime', 'type', 'price', 'units']
+        df = pd.DataFrame(columns=keys)
+
+        for _, row in raw_df.iterrows():
+            if row['type'] in ['buy', 'sell']:
+                decoupled_df = pd.DataFrame([[row['datetime'], row['type'], row['price'], row['units'] * (-1 if row['type'] == 'sell' else 1)],
+                                              [row['datetime'], 'fee', row['price'], -row['fee']]], 
+                                             columns=keys)
+            elif row['type'] == 'comm':
+                decoupled_df = pd.DataFrame([[row['datetime'], row['type'], row['price'], row['units']]], columns=keys)
+
+            elif row['type'] == 'transfer':
+                decoupled_df = pd.DataFrame([[row['datetime'], 'fee', row['price'], -row['fee']]], columns=keys)
+
+            # Append to main df
+            df = df.append(decoupled_df)
+
+        # Make 'type' to categorical
+        df['type'] = df['type'].astype('category').cat.set_categories(Currency.transaction_types)
+
+        # Reset index to be a monotonically increasing range
+        df = df.reset_index(drop=True)
+
+        return df
+
+    def roi(self, duration, mode='money'):
+        ''' DataFrame, str -> float, bool
+        Calculate a given `mode` of return of investment. 
+        ROI is annualized if the duration is over a year. 
+
+        Simple ROI: Final Market Value / Initial Market Value
+        Time ROI: Time weighted return
+        Money ROI: Modified Dietz method
+
+        Inputs:
+            mode: Type of ROI to calculate. Valid options are ['simple', 'time', 'money']
+            duration: String denoting appropriate duration. 
+                          Follows pd time offsets. Multiples allowed.
+                          Currently implemented for ['D', 'W', 'M', 'Q', 'Y', 'MTD', 'YTD'].
+
+        Returns:
+            ROI (annualized if time frame is over a year).
+            Boolean on whether ROI was annualized.
+        '''
+        def _endpoint_dates(duration, self):
+            ''' str -> Date, Date
+            Return the date `duration` in the past as well as the current date.
+            '''
+            # Separate potential multiple from time unit
+            time_unit = re.search(r'[A-Z]+$', duration).group()
+
+            time_multiple = duration.split(time_unit)[0]
+            time_multiple = 1 if time_multiple == '' else int(time_multiple)
+
+            # Ensure that `duration` is valid
+            assert time_unit in ['D', 'W', 'M', 'Q', 'Y', 'MTD', 'YTD'], "Invalid duration provided. Choose from ['D', 'W', 'M', 'Q', 'Y', 'MTD', 'YTD']."
+
+            # Short circuit function it is `time_unit` is 'YTD'
+            if time_unit in ['MTD', 'YTD']:
+                end = datetime.datetime.now()
+                start = datetime.datetime(end.year, 1 if time_unit == 'YTD' else end.month, 1)
+                return start, end
+
+            # Dictionary whose values are binary lists of (day, month, year)
+            DAYS_IN_DURATION = {
+                'D': [1, 0, 0],
+                'W': [7, 0, 0],
+                'M': [0, 1, 0],
+                'Q': [0, 3, 0],
+                'Y': [0, 0, 1]
+            }
+
+            # Get `days`, `months` and `years` variables for use in relativedelta
+            days, months, years = [-time_multiple * x for x in DAYS_IN_DURATION[time_unit]]
+
+            # Calculate start and end dates
+            end = datetime.datetime.now()
+            start = max(self.inception_date, end + relativedelta(days=days, months=months, years=years))
+
+            return start, end
+
+        def _add_endpoints(df, duration, self):
+            ''' DataFrame, str, Asset -> DataFrame
+            Append and prepend a row each to `df` for the current date
+            and a date in the past by `duration` respectively.
+
+            Inputs:
+                df:       DataFrame with pre-computed values
+                duration: String denoting appropriate duration. 
+                          Follows pd time offsets. Multiples allowed.
+                          Currently implemented for ['D', 'W', 'M', 'Q', 'Y', 'MTD', 'YTD'].
+                obj:      Required to infer prices of Asset
+            '''
+            # Get start and end dates
+            start, end = _endpoint_dates(duration, self)
+
+            # Add a row for start date if inception date precedes it
+            if start > self.inception_date:
+                df = df.append(pd.DataFrame({
+                        'datetime': [start],
+                        'type': pd.Categorical(['display'], categories=Currency.transaction_types),
+                        'price': [Currency.price(self.symbol, timestamp=start.timestamp())],
+                        'units': [0],
+                        'unit_balance': [df.at[0, 'unit_balance'] - df.at[0, 'units']]
+                    }))
+
+            # Add a row for end date, sort the dates and columns
+            df = (
+                df
+                .append(pd.DataFrame({
+                    'datetime': [end],
+                    'type': pd.Categorical(['display'], categories=Currency.transaction_types),
+                    'price': [Currency.price(self.symbol, timestamp=end.timestamp())],
+                    'units': [0],
+                    'unit_balance': [df['unit_balance'].iat[-1]]}))
+                .sort_values('datetime')
+                .reset_index(drop=True)
+            )[['datetime', 'type', 'price', 'units', 'unit_balance']]
+
+            return df
+
+        def _roi_pre_computations(self, duration):
+            ''' DataFrame, str -> DataFrame
+            Return a DataFrame containing the necessary computations for ROI calculations.
+            '''
+            # Decouple fees from transactions
+            df = (
+                self
+                .transactions
+                .pipe(Currency._decouple_fees)
+                .assign(unit_balance = lambda df: df['units'].cumsum())
+            )
+
+            # Filter rows with relevant dates
+            df = df[df['datetime'] >= _endpoint_dates(duration, self)[0]]
+
+            assert len(df) > 0, 'No transactions for this time window.'
+
+            # Add start and end rows
+            # Calculate remainder of states
+            df = (
+                df
+                .reset_index(drop=True)
+                .pipe(_add_endpoints, duration, self)
+                .assign(cashflow = lambda df: df['units'] * df['price'])
+                .assign(value = lambda df: df['unit_balance'] * df['price'])
+                .assign(inter_dt_roi = lambda df: df['unit_balance'].shift(1) * df['price'] / df['value'].shift(1) - 1)
+                .assign(cum_days_delta = lambda df: (df['datetime'] - df.at[0, 'datetime']).apply(lambda dt: dt.days))
+                .assign(dietz_weight = lambda df: 1 - df['cum_days_delta'] / df['cum_days_delta'].iat[-1])
+            )
+
+            # Replace NaN values with 0.
+            df.loc[0, 'inter_dt_roi':'cum_days_delta'] = 0
+
+            return df
+        
+        
+        ###################
+        # Start of method #
+        ###################
+
+        # Ensure that mode is valid
+        assert mode in ['simple', 'time', 'money'], 'Invalid mode provided. Choose from "simple", "time" and "money".'
+        
+        # Get precomputed dataframe
+        df = _roi_pre_computations(self, duration)
+
+        # Constants
+        _DP = 5 # Decimal places for rounding
+        _EPSILON = 1e-10 # For numerical stability
+
+        # Infer length of `df`
+        N = len(df)
+
+        # Infer duration of dataset
+        total_days = (df.at[N - 1, 'datetime'] - df.at[0, 'datetime']).days
+
+        if mode == 'money':
+            _roi = (df.at[N - 1, 'value'] - df.at[0, 'value'] - df['cashflow'].sum()) / (df.at[0, 'value'] + (df['dietz_weight'] * df['cashflow']).sum() + _EPSILON)
+        elif mode == 'time':
+            _roi = (df['inter_dt_roi'] + 1).agg(np.prod) - 1
+        elif mode == 'simple':
+            _roi = df.at[N - 1, 'value'] / df.at[0, 'value'] - 1
+
+        # Annualize ROI if it over a year
+        if total_days > 365.25:
+            _roi = (_roi + 1)**(365.25 / (total_days + _EPSILON)) - 1
+
+        # Round to appropriate decimal places
+        _roi = round(_roi, _DP)
+
+        return _roi, total_days > 365.25
     
 class Crypto(Currency):
     def price(self, fiat='cad'):
